fe4b1b5cf522538050cf2f17030c4ce8aded437e
Mihail Tsankov <mihail.tsankov@trading212.com>
2024-02-17 17:42:27 +0200

    nice cycle

--- scenes/level/Level.py
+++ scenes/level/Level.py
@@ -4,7 +4,9 @@ from component.Gear.GearMouse import GearMouse
 from component.Robot.Robot import Robot
 from component.AssertGate.AssertGate import AssertGate
 from scenes.level.LevelBase import LevelBase
-from consts.game import FPS, BACKGROUND_COLOR
+from scenes.transitions.Success import Success
+from scenes.transitions.Restart import Restart
+from consts.game import FPS, BACKGROUND_COLOR, SECOND
 
 pygame.init()
 
@@ -21,36 +23,67 @@ player_commands = [
 class Level:
     def __init__(self, screen, UI_manager):
         self.screen = screen
-        self.mouse = GearMouse.get_instance(screen)
         self.manager = UI_manager 
+        self.__base_init()
 
-        self.robot = Robot(screen, tile=(1, 1), 
-                           health=10, 
-                           on_death=lambda: print("Robot is dead"))
+    def __base_init(self):
+        self.mouse = GearMouse.get_instance(self.screen)
+        self.show_success = Success(self.screen)
+        self.show_restart = Restart(self.screen)
+        self._restart_timer = 0
+        self._success_timer = 0
+
+        self.robot = Robot(self.screen, tile=(1, 1), 
+                           health=1, 
+                           on_death = self.__on_fail)
 
         self.level = LevelBase(
-            screen, 
+            self.screen, 
             'Hello World!\n\nThis is a text box\nIt can be used to display text\nIt can also be used to display HTML\n<font color=#FF0000>Like this</font>',
             self.manager,
             player=self.robot,
             commands=player_commands
             )
        
-        self.assertGate = AssertGate(screen, 
+        self.assertGate = AssertGate(self.screen, 
                                      self.robot, 
                                      lambda robot: True, 
                                      tile=(8, 1),
-                                     on_fail=lambda: print("Fail"), 
-                                     on_pass=lambda: print("Pass")
-                                     )
+                                     on_pass = self.__on_success,
+                                     on_fail = self.__on_fail)
+
+
+    def __on_fail(self):
+        self._restart_timer = SECOND
+        self._success_timer = 0
+    
+    def __on_success(self):
+        self._success_timer = SECOND
+        self._restart_timer = 0
+    
+    def __handle_transitions(self):
+        ans = self.__handle_transition('_restart_timer')
+        if ans != 0:
+            return ans
+        return self.__handle_transition('_success_timer')        
+    
+    def __handle_transition(self, transition):
+        val = getattr(self, transition)
+        if val > 0:
+            setattr(self, transition, val - 1)
+            if val - 1 == 0:
+                self.show_restart()
+                return 2
+            return 1
+        return 0
 
     def set_on_back(self, on_back):
         self.level.set_on_back(on_back)
+        self.show_success.set_on_button_press(on_back)
 
     def __update_entities(self):
         self.assertGate.update()
         self.robot.update()
-
 
     def __update(self, time_delta):
         self.mouse.update()
@@ -69,12 +102,17 @@ class Level:
 
                 self.level.handle_input(event)
                 self.manager.process_events(event)
-
+            
+            if self.__handle_transitions() == 2:
+                return
+            
             self.level.update()
             self.__update_entities()
             self.__update(time_delta)
 
     def __call__(self, level):
         self.screen.fill(BACKGROUND_COLOR)
+        self.__base_init()
+        self.show_restart.set_on_button_press(lambda: self.__call__(level))
         self.__init()
               
---
+++ scenes/transitions/Restart.py
@@ -0,0 +1,38 @@
+import pygame
+
+from component.backgrounds.MenuBackground import MenuBackground
+from component.buttons.TextButton import TextButton
+from component.Gear.GearMouse import GearMouse
+from consts.game import SCREEN_WIDTH, SCREEN_HEIGHT, BACKGROUND_COLOR, FPS
+
+class Restart:
+    def __init__(self, screen):
+        self.screen = screen
+        self.background = MenuBackground(screen)
+        self.mouse = GearMouse.get_instance(screen)
+        self.restart_button = TextButton(screen, 
+                                       'Loser! Try again!', 
+                                       (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), 
+                                       text_color=(250, 0, 20))
+
+    def set_on_button_press(self, on_button_press):
+        self.restart_button.set_on_click(on_button_press)
+
+    def __update(self):
+        self.screen.fill(BACKGROUND_COLOR)
+        self.restart_button.update()
+        self.mouse.update()
+        pygame.display.update()
+
+    def __init(self):
+        clock = pygame.time.Clock()
+
+        while True:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            self.__update()
+
+    def __call__(self):
+        self.__init()
